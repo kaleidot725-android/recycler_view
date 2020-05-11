ca55ed3f81349cbc0194fe71478055bf33a508a8
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-12 07:15:04 +0900

    Add StateRestorationPolicy

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -2,6 +2,9 @@ package jp.kaleidot725.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,9 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.apply {
-            this.adapter = CustomAdapter(listOf("A", "B", "C", "D", "E"))
-            this.layoutManager = LinearLayoutManager(context).also { lm -> lm.orientation = RecyclerView.VERTICAL }
+        val recyclerViewAdapter = CustomAdapter(SAMPLE_DATA).also { adapter ->
+            adapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT
         }
+        recycler_view.adapter = recyclerViewAdapter
+
+        val recyclerViewLayoutManager =  LinearLayoutManager(applicationContext).also {
+            lm -> lm.orientation = RecyclerView.VERTICAL
+        }
+        recycler_view.layoutManager = recyclerViewLayoutManager
+    }
+
+    companion object {
+        private val SAMPLE_DATA = listOf("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
     }
 }
